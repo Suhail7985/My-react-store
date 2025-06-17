92687ef5ac3d894e775d5aa44680df30554ba7ca
Suhail7985 <suhailmansoori7985@gmail.com>
2025-06-17 16:53:05 +0530

    first commit-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-function App() {
+import Home from './Components/Home'
+import Login from './Components/Login'
 
+function App() {
   return (
     <>
       <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="Login" element={<Login/>} />
-
-
-
-      </Routes>
-      <Footer/>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-        
     </>
   )
 }
